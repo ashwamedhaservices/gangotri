a2559eebdedccd8172cf3d581c54d289cf92875d
Ravindra Kumawat <ravindra.kumawat@kuvera.in>
2023-07-30 01:36:11 +0530

    routing changed

--- src/context/authentication/authContextProvider.js
+++ src/context/authentication/authContextProvider.js
@@ -34,7 +34,7 @@ export const AuthContextProvider = (props) => {
           password
         }
       })
-      navigate('/dashboard', { replace: true });
+      navigate('/', { replace: true });
     }
     // if(user) {
     //   dispatch(
