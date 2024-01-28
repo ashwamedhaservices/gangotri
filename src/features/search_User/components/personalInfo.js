a2ecc412373d7bb458e76d56d93c6bea332ebdc9
Sandy <sandeshrnaik2000@gmail.com>
2024-01-29 02:25:57 +0530

    User Search Page

--- src/features/search_User/components/personalInfo.js
+++ src/features/search_User/components/personalInfo.js
@@ -6,76 +6,63 @@ import {
   MenuItem,
   Button,
   Grid,
-  Card,
-  CardContent,
 } from "@mui/material";
-import { styled } from "@mui/system";
-
-const StyledCard = styled(Card)({
-  backgroundColor: "darkwhite",
-  marginTop: "20px",
-  padding: "10px",
-});
+import { useLocation } from "react-router-dom";
 
 function PersonalInfo() {
   const [selectedRole, setSelectedRole] = useState("admin");
+  const location = useLocation();
+  const user = location.state?.user;
 
   const handleRoleChange = (event) => {
     setSelectedRole(event.target.value);
   };
 
-  const userData = {
-    id: 123,
-    name: "Sandesh",
-    email: "sandeshrnaik2000@gmail.com",
-    CurrentRole: "User",
-  };
-
   return (
-    <>
-      <div>
-        <h5 style={{ textAlign: "center" }}>User Name</h5>
-        <h3>Personal-information</h3>
+    <div style={{ marginLeft: "20px" }}>
+      <h5 style={{ textAlign: "center" }}>
+        {user
+          ? `${user.fname} ${user.mname ? user.mname + " " : ""}${user.lname}`
+          : "Loading..."}
+      </h5>
+      <h3>Personal Information</h3>
+      <div style={{ display: "grid", gridTemplateColumns: "repeat(2, 1fr)" }}>
+  {user &&
+    Object.entries(user).map(([key, value], index) => (
+      <div
+        key={key}
+        style={{
+          marginBottom: "10px",
+          gridColumn: index % 2 === 0 ? "1" : "2", 
+        }}
+      >
+        <span style={{ fontWeight: "bold" }}>{key}:</span> {value}
       </div>
-      <StyledCard>
-        <CardContent>
-          <Grid item xs={12}>
-            <div>
-              User Data:
-              <ul>
-                <li>ID: {userData.id}</li>
-                <li>Name: {userData.name}</li>
-                <li>Email: {userData.email}</li>
-                <li>User Role: {userData.CurrentRole}</li>
-              </ul>
-            </div>
-          </Grid>
-          <Grid item xs={12} marginTop={5}>
-            <h3>Upgrade Role</h3>
-            <FormControl >
-              <InputLabel id="role-label">Select Role:</InputLabel>
-              <Select
-                labelId="role-label"
-                id="role"
-                value={selectedRole}
-                onChange={handleRoleChange}
-                label="Select Role"
-              >
-                <MenuItem value="admin">Admin</MenuItem>
-                <MenuItem value="fieldOfficer">
-                  Field Development Officer
-                </MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} marginTop={3}>
-            <Button type="submit" variant="contained" color="primary" >
-              Update
-            </Button>
-          </Grid>
-        </CardContent>
-      </StyledCard>
-    </>
+    ))}
+</div>
+
+      <Grid item xs={12} style={{ marginTop: "3px" }}>
+        <h3>Upgrade Role</h3>
+        <FormControl>
+          <InputLabel id="role-label">Select Role:</InputLabel>
+          <Select
+            labelId="role-label"
+            id="role"
+            value={selectedRole}
+            onChange={handleRoleChange}
+            label="Select Role"
+          >
+            <MenuItem value="admin">Admin</MenuItem>
+            <MenuItem value="fieldOfficer">Field Development Officer</MenuItem>
+          </Select>
+        </FormControl>
+      </Grid>
+      <Grid item xs={12} style={{ marginTop: "10px" }}>
+        <Button type="submit" variant="contained" color="primary">
+          Update
+        </Button>
+      </Grid>
+    </div>
   );
 }
 
--- src/features/search_User/components/search-user.js
+++ src/features/search_User/components/search-user.js
@@ -1,20 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import FormControl from '@mui/material/FormControl';
-import { OutlinedInput } from '@mui/material';
-import { useNavigate } from "react-router-dom";
-import { Table,TableBody, TableCell, TableContainer, TableHead, TableRow, } from '@mui/material';
+import { OutlinedInput, Snackbar } from '@mui/material';
+import { Alert } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 
 const SearchUser = () => {
   const [userId, setUserId] = useState('');
   const [mobileNumber, setMobileNumber] = useState('');
   const [referralCode, setReferralCode] = useState('');
   const [isMatched, setIsMatched] = useState(false);
+  const [userData, setUserData] = useState([]);
+  const [matchedUser, setMatchedUser] = useState(null);
+  const [alertOpen, setAlertOpen] = useState(false);
   const navigate = useNavigate();
-  const data = [
-    { userId: 1, name:'Sandesh',mobileNumber: '9900646757', email: 'sandeshrnaik2000@gmail.com', referralCode: 'ABC123' },
-    
-   
-  ];
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          'https://apistage.ashwamedha.net/staging/accounts/admin/api/v1/users.json',
+          {
+            headers: {
+              Authorization:
+                'Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MTIsInByb2ZpbGVfaWQiOjEyfQ.KXMZMWfmnMPDbWhF9ale1XyfEdJAYI5mpY7P4Br_DpM',
+            },
+          }
+        );
+        const data = await response.json();
+        setUserData(data.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   const handleUserIdChange = (event) => {
     setUserId(event.target.value);
   };
@@ -34,19 +56,30 @@ const SearchUser = () => {
   };
 
   const handleEnterClick = () => {
-    
-    if (userId === 'backendUserId' || mobileNumber === 'backendMobileNumber' || referralCode === 'backendReferralCode') {
+    const user = userData.find((user) => {
+      return (
+        user.id === parseInt(userId) ||
+        user.mobile_number === mobileNumber ||
+        user.referral_code === referralCode
+      );
+    });
+
+    if (user) {
       setIsMatched(true);
-      setUserId('');
-      setMobileNumber('');
-      setReferralCode('');
+      setMatchedUser(user);
     } else {
-      setIsMatched(true);
-      setUserId('');
-      setMobileNumber('');
-      setReferralCode('');
-      
+      setIsMatched(false);
+      setMatchedUser(null);
+      setAlertOpen(true);
     }
+
+    setUserId('');
+    setMobileNumber('');
+    setReferralCode('');
+  };
+
+  const handleAlertClose = () => {
+    setAlertOpen(false);
   };
 
   return (
@@ -55,8 +88,8 @@ const SearchUser = () => {
         <FormControl sx={{ m: 1, width: '30vw' }} variant="outlined">
           <h4>Enter User ID</h4>
           <OutlinedInput
-            type='text'
-            placeholder='Enter User ID'
+            type="text"
+            placeholder="Enter User ID"
             value={userId}
             onChange={handleUserIdChange}
             onKeyPress={handleEnterKeyPress}
@@ -65,8 +98,8 @@ const SearchUser = () => {
         <FormControl sx={{ m: 1, width: '30vw' }} variant="outlined">
           <h4>Mobile Number</h4>
           <OutlinedInput
-            type='text'
-            placeholder='Enter Mobile Number'
+            type="text"
+            placeholder="Enter Mobile Number"
             value={mobileNumber}
             onChange={handleMobileNumberChange}
             onKeyPress={handleEnterKeyPress}
@@ -75,43 +108,56 @@ const SearchUser = () => {
         <FormControl sx={{ m: 1, width: '30vw' }} variant="outlined">
           <h4>Referral Code</h4>
           <OutlinedInput
-            type='text'
-            placeholder='Enter Referral Code'
+            type="text"
+            placeholder="Enter Referral Code"
             value={referralCode}
             onChange={handleReferralCodeChange}
             onKeyPress={handleEnterKeyPress}
           />
         </FormControl>
       </div>
-      {isMatched && <TableContainer sx={{margin:'5vh',width: '90%', }} >
-      <Table  >
-        <TableHead>
-          <TableRow >
-            <TableCell>User ID</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Mobile Number</TableCell>
-            <TableCell>Email</TableCell>
-            <TableCell>Referral Code</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody  onClick={()=>{navigate('/search-user/personalInfo', { replace: true })}}>
-          {data.map((row) => (
-            <TableRow key={row.userId} >
-              <TableCell>{row.userId}</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.mobileNumber}</TableCell>
-              <TableCell>{row.email}</TableCell>
-              <TableCell>{row.referralCode}</TableCell>
-            </TableRow>
-          ))
-          }
-        </TableBody>
-      </Table>
-    </TableContainer> }
-    
+      {isMatched && matchedUser && (
+        <>
+          <TableContainer sx={{ margin: '5vh', width: '90%' }}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>User ID</TableCell>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Mobile Number</TableCell>
+                  <TableCell>Email</TableCell>
+                  <TableCell>Referral Code</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody
+                onClick={() =>
+                  navigate('/search-user/personalInfo', { state: { user: matchedUser } })
+                }
+              >
+                <TableRow>
+                  <TableCell>{matchedUser.id}</TableCell>
+                  <TableCell>{`${matchedUser.fname} ${matchedUser.lname}`}</TableCell>
+                  <TableCell>{matchedUser.mobile_number}</TableCell>
+                  <TableCell>{matchedUser.email}</TableCell>
+                  <TableCell>{matchedUser.referral_code}</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </>
+      )}
+      <Snackbar
+        open={alertOpen}
+        autoHideDuration={4000}
+        onClose={handleAlertClose}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+      >
+        <Alert onClose={handleAlertClose} severity="error" sx={{ width: '100%' }}>
+          User not found!
+        </Alert>
+      </Snackbar>
     </>
-     // navigate('/search-user/personalInfo', { replace: true })
   );
-}
+};
 
 export default SearchUser;
