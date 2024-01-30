c9e02576afbdd35820b642c73d0423fbe2acd210
Sandy <sandeshrnaik2000@gmail.com>
2024-01-31 02:55:59 +0530

    User Details tabs are added

--- src/features/search_User/components/personalInfo.js
+++ src/features/search_User/components/personalInfo.js
@@ -1,21 +1,11 @@
-import React, { useState } from "react";
-import {
-  FormControl,
-  InputLabel,
-  Select,
-  MenuItem,
-  Button,
-  Grid,
-} from "@mui/material";
 import { useLocation } from "react-router-dom";
+import UserDetails from './userDetails';
 
 function PersonalInfo() {
-  const [selectedRole, setSelectedRole] = useState("admin");
   const location = useLocation();
   const user = location.state?.user;
-
-  const handleRoleChange = (event) => {
-    setSelectedRole(event.target.value);
+  const filterProperties = (key) => {
+    return !["salt", "password_digest"].includes(key);
   };
 
   return (
@@ -27,41 +17,24 @@ function PersonalInfo() {
       </h5>
       <h3>Personal Information</h3>
       <div style={{ display: "grid", gridTemplateColumns: "repeat(2, 1fr)" }}>
-  {user &&
-    Object.entries(user).map(([key, value], index) => (
-      <div
-        key={key}
-        style={{
-          marginBottom: "10px",
-          gridColumn: index % 2 === 0 ? "1" : "2", 
-        }}
-      >
-        <span style={{ fontWeight: "bold" }}>{key}:</span> {value}
+        {user &&
+          Object.entries(user)
+            .filter(([key]) => filterProperties(key))
+            .map(([key, value], index) => (
+              <div
+                key={key}
+                style={{
+                  marginBottom: "10px",
+                  gridColumn: index % 2 === 0 ? "1" : "2",
+                }}
+              >
+                <span style={{ fontWeight: "bold" }}>{key}:</span>
+               {value !== "" ? value : "null"}
+      
+              </div>
+            ))}
       </div>
-    ))}
-</div>
-
-      <Grid item xs={12} style={{ marginTop: "3px" }}>
-        <h3>Upgrade Role</h3>
-        <FormControl>
-          <InputLabel id="role-label">Select Role:</InputLabel>
-          <Select
-            labelId="role-label"
-            id="role"
-            value={selectedRole}
-            onChange={handleRoleChange}
-            label="Select Role"
-          >
-            <MenuItem value="admin">Admin</MenuItem>
-            <MenuItem value="fieldOfficer">Field Development Officer</MenuItem>
-          </Select>
-        </FormControl>
-      </Grid>
-      <Grid item xs={12} style={{ marginTop: "10px" }}>
-        <Button type="submit" variant="contained" color="primary">
-          Update
-        </Button>
-      </Grid>
+      <UserDetails userId={user.id} />
     </div>
   );
 }
